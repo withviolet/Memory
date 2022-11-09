4d12f9da399df8b31eb48ada9e489555a1848c77
withviolet <1071069722@qq.com>
2022-11-09 15:00:13 +0800

    创建新项目

---
+++ Memory/Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by paintitcolorful on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
