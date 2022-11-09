e03e806a6b277d1fa277a7b4ad950aa5f2eac23b
withviolet <1071069722@qq.com>
2022-11-09 15:33:12 +0800

    点击卡牌可翻转

--- Memory/DerivedData/Memory/Build/Intermediates.noindex/Previews/Memory/Intermediates.noindex/Memory.build/Debug-iphonesimulator/Memory.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
+++ Memory/DerivedData/Memory/Build/Intermediates.noindex/Previews/Memory/Intermediates.noindex/Memory.build/Debug-iphonesimulator/Memory.build/Objects-normal/arm64/ContentView.1.preview-thunk.swift
@@ -4,8 +4,33 @@ import SwiftUI
 
 extension ContentView_Previews {
     @_dynamicReplacement(for: previews) private static var __preview__previews: some View {
-        #sourceLocation(file: "/Users/paintitcolorful/StudySpace/Memory/Memory/Memory/ContentView.swift", line: 24)
+        #sourceLocation(file: "/Users/paintitcolorful/StudySpace/Memory/Memory/Memory/ContentView.swift", line: 46)
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    
+#sourceLocation()
+    }
+}
+
+extension CardView {
+    @_dynamicReplacement(for: body) private var __preview__body: some View {
+        #sourceLocation(file: "/Users/paintitcolorful/StudySpace/Memory/Memory/Memory/ContentView.swift", line: 27)
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: __designTimeInteger("#6801.[2].[1].property.[0].[0].arg[0].value.[0].value.arg[0].value", fallback: 20))
+            if faceUp{
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: __designTimeInteger("#6801.[2].[1].property.[0].[0].arg[0].value.[1].[0].[1].modifier[0].arg[0].value", fallback: 3))
+                Text(__designTimeString("#6801.[2].[1].property.[0].[0].arg[0].value.[1].[0].[2].arg[0].value", fallback: "✈️")).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            faceUp = !faceUp
+        }
+        
     
 #sourceLocation()
     }
@@ -14,10 +39,11 @@ extension ContentView_Previews {
 extension ContentView {
     @_dynamicReplacement(for: body) private var __preview__body: some View {
         #sourceLocation(file: "/Users/paintitcolorful/StudySpace/Memory/Memory/Memory/ContentView.swift", line: 12)
-        ZStack {
-            RoundedRectangle(cornerRadius: __designTimeInteger("#6801.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: 20))
-                .stroke(lineWidth: __designTimeInteger("#6801.[1].[0].property.[0].[0].arg[0].value.[0].modifier[0].arg[0].value", fallback: 3))
-            Text(__designTimeString("#6801.[1].[0].property.[0].[0].arg[0].value.[1].arg[0].value", fallback: "H"))
+        HStack {
+            CardView(faceUp: __designTimeBoolean("#6801.[1].[0].property.[0].[0].arg[0].value.[0].arg[0].value", fallback: true))
+            CardView()
+            CardView()
+            CardView() // 在这里设定的初始值，的权限高于在CardView body中的
         }
         .padding(.horizontal)
         .foregroundColor(.red)
@@ -27,4 +53,5 @@ extension ContentView {
 }
 
 import struct Memory.ContentView
+import struct Memory.CardView
 import struct Memory.ContentView_Previews
--- Memory/Memory/ContentView.swift
+++ Memory/Memory/ContentView.swift
@@ -4,23 +4,48 @@
 //
 //  Created by paintitcolorful on 2022/11/9.
 //
-
+// 大量的小视图，是swiftUI的设计范式
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("H")
+        HStack {
+            CardView(faceUp: true)
+            CardView()
+            CardView()
+            CardView() // 在这里设定的初始值，的权限高于在CardView body中的
         }
         .padding(.horizontal)
         .foregroundColor(.red)
     }
 }
 
+struct CardView: View {
+    @State var faceUp: Bool = false // 卡牌是否面朝上
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if faceUp{
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            faceUp = !faceUp
+        }
+        
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
