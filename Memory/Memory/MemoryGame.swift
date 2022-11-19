6e98fd0965654463ef7544a3b69ad337ad9c428e
withviolet <1071069722@qq.com>
2022-11-19 12:01:37 +0800

    Lecture 4 card匹配

--- Memory/Memory/MemoryGame.swift
+++ Memory/Memory/MemoryGame.swift
@@ -7,24 +7,44 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     
     private(set) var cards: Array<Card>
     
+    private var indexOfCardOnlyFaceUp: Int?
+    
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
-        print("选中的card: \(cards[chosenIndex].content)")
+//        guard let chosenIndex = index(of: card) else { return }
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }),
+            !cards[chosenIndex].isMatched,
+            !cards[chosenIndex].isFaceUp {
+            
+            if let protencialCardIndex = indexOfCardOnlyFaceUp {
+                if cards[protencialCardIndex].content == cards[chosenIndex].content {
+                    cards[protencialCardIndex].isMatched = true
+                    cards[chosenIndex].isMatched = true
+                }
+                indexOfCardOnlyFaceUp = nil
+            } else {
+                for index in cards.indices {
+                    cards[index].isFaceUp = false
+                }
+                indexOfCardOnlyFaceUp = chosenIndex
+            }
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
     
-    func index(of card: Card)-> Int {
-        for index in 0 ..< cards.count {
-            if cards[index].id == card.id {
-                return index
-            }
-        }
-        return 0
-    }
+    
+    // firstIndex where 可以替代这个函数
+//    func index(of card: Card)-> Int? {
+//        for index in 0 ..< cards.count {
+//            if cards[index].id == card.id {
+//                return index
+//            }
+//        }
+//        return nil
+//    }
     
     init(numbersOfPairsOfCards: Int, createCardContent: (Int) -> CardContent ) {
         
@@ -39,7 +59,7 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable{
-        var isFaceUp: Bool = true
+        var isFaceUp: Bool = false
         var isMatched: Bool = false
         var content: CardContent
         var id: Int
